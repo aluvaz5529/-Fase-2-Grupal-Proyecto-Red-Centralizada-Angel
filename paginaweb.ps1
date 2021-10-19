32c6063cfe11961027171bd92789c5a06a218c5c
aluvaz5529 <91330510+aluvaz5529@users.noreply.github.com>
2021-10-19 09:31:20 +0200

    Create paginaweb.ps1

---
+++ paginaweb.ps1
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Universidad de Castellon</title>
+<meta charset="UTF-8">
+<p>Angel Vazquez Saura</p>
+<style>
+	h1 {color: red; background-color: #d8e4f4}
+</style>
+</head>
+<body>
+
+	<a href=""><h1 align="center">Campus de Castellon</h1><a>
+	<img src="https://lh3.googleusercontent.com/proxy/ridZoB15Y0-uP677_24P9GpZrdjoW-v7NiPJjKPTi8l6XCFnUFDhr_G6eXOhgAROQQq59m5V8pzMSuvpkDe1Geh0k8FWKPMvHNYJzVH0tKyJS1H38KPyb5IUJQ"
+		width="100" height="100"/>
+<hr>
+<p>universidad de castellon</p>
+<p>informatica</p>
+<p>universidad de castellon</p>
+<p>universidad de castellon</p>
+
+</body>
+</html>
